ed9b948bd2272ef6c32d668a3d15d12a9d60df98
love-sick <ssajad2153@gmail.com>
2016-06-16 02:46:18 -0700

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -226,7 +226,7 @@ function create_config( )
     "leave_ban",
     "admin"
     },
-    sudo_users = {sajad2153},--Sudo users
+    sudo_users = {199947072},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v2 - Open Source
